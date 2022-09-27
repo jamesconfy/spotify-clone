9dd28f7889bc20f14bc7dc822f4217b729332b6f
James Confidence <63427518+jamesconfy@users.noreply.github.com>
2022-09-27 13:43:07 +0100

    Initial commit

---
+++ src/pages/Discover.jsx
@@ -0,0 +1,40 @@
+/* eslint quotes: ["error", "double"] */
+
+import { Error, Loader, SongCard } from "../components";
+
+import { genres } from "../assets/constants";
+
+const Discover = () => {
+  const genreTitle = "Pop";
+
+  return (
+    <div className="flex flex-col">
+      <div className="w-full flex justify-between items-center sm:flex-row flewx-col mt-4 mb-10">
+        <h2 className="font-bold text-3xl text-left text-zinc-300">
+          Discover {genreTitle}
+        </h2>
+        <select
+          onChange={(e) => {
+            e.preventDefault();
+          }}
+          value=""
+          className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
+        >
+          {genres.map((genre) => (
+            <option key={genre.value} value={genre.value}>
+              {genre.title}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+          <SongCard key={song.key} i={i} song={song} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Discover;
